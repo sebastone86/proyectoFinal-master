2744342999ae232edc65a721e3902b23f4ec913f
AlexisFrisone <alexisfrisone@gmail.com>
2019-09-11 16:12:52 -0300

    -Empezando interfaces api  (con error )

--- app/src/main/java/com/example/proyectofinal/ui/login/Model/IApiServices.kt
+++ app/src/main/java/com/example/proyectofinal/ui/login/Model/IApiServices.kt
@@ -1,6 +1,7 @@
 package com.example.proyectofinal.ui.login.Model
 
 import android.telecom.Call
+import com.example.proyectofinal.data.model.Usuario
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -8,15 +9,14 @@ import retrofit2.http.Path
 
 interface IApiServices {
 
+    /* Usuario */
+    @GET("user/")
+    fun getAllPosts(): Call(List<Usuario>)
 
-    @GET("posts/")
-    fun getAllPosts(): Call<List<Post>>
+    @GET( "user/{id}" )
+    fun getPostById(@Path("id") id:Int ) :Call(var u :Usuario )
 
-
-    @GET( "posts/{id}" )
-    fun getPostById(@Path("id") id:Int ) :Call<Post>
-
-    @POST("posts/{id}")
-    fun editPostById (@Path ("id") id:Int , @Body post: Post?) :Call<Post>
+    @POST("user/{id}")
+    fun editPostById (@Path ("id") id:Int , @Body post: Usuario?) :Call<Usuario>
 
 }
