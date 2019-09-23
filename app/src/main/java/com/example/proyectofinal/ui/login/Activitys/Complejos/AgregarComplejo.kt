dbfdf66e0f9bde16e054579eb069205e790d1190
Wyso <eric.wysoczanski@gmail.com>
2019-09-23 17:00:04 -0300

    - Modificaciones simples y AgregarComplejoActivity

---
+++ app/src/main/java/com/example/proyectofinal/ui/login/Activitys/Complejos/AgregarComplejo.kt
@@ -0,0 +1,13 @@
+package com.example.proyectofinal.ui.login.Activitys.Complejos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.proyectofinal.R
+
+class AgregarComplejo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_complejo)
+    }
+}
